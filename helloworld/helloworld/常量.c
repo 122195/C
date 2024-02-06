16bfaec278f4a50245595dc776d95ad031cc88c2
暴龙战士 <63136115+122195@users.noreply.github.com>
2024-02-06 16:33:55 +0800

    2024

---
+++ helloworld/helloworld/常量.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//int main()
+//{
+	// 输出常量
+	// 1.输出整数
+	//printf("%d", 18);
+	//// 2.输出小数，保留小数点后六位
+	//printf("\n%f", 181.6);
+	//// 3.输出字符
+	//printf("\n%c", 'A');
+	//// 4.输出字符串
+	//// 第一种方式
+	//printf("\nhelloworld");
+	//// 第二种方式
+	//printf("\nmyname:%s", "hank");
+	//return 0;
+//}
