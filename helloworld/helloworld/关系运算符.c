6b9342bf66bc8d90975fa4e1247773cee673c97b
暴龙战士 <63136115+122195@users.noreply.github.com>
2024-02-16 23:42:14 +0800

    2024-2-16

---
+++ helloworld/helloworld/关系运算符.c
@@ -0,0 +1,15 @@
+//#include <stdio.h>
+//
+//int main()
+//
+//{
+//	int a = 10;
+//	int b = 20;
+//	// 真表示1 假表示0
+//	printf("%d\n", a == b); // 等于
+//	printf("%d\n", a != b); // 不等于
+//	printf("%d\n", a > b); // 大于
+//	printf("%d\n", a >= b); // 大于等于
+//	printf("%d\n", a < b); // 小于
+//	printf("%d\n", a <= b); // 小于等于
+//}
--- helloworld/helloworld/键盘录入的基本使用.c
+++ helloworld/helloworld/键盘录入的基本使用.c
@@ -1,24 +1,24 @@
-
-#include <stdio.h>
-
-int main()
-{
-	// 键盘录入的基本使用
-	// 1.定义一个变量用来接收数据
-	int a;
-	// 2.键盘录入
-	printf("请输入一个整数");
-	scanf("%d", &a);
-	// 3.打印一下
-	printf("变量a里面的值是：%d\n", a);
-	//char b[]="你好";
-	//printf("%s\n", b);
-	// 定义两个整数类型的变量num1和num2，键盘录入数据分别为两个变量赋值，求两个数的和并进行打印
-	/*int num1;
-	int num2;
-	printf("请输入第一个整数");
-	scanf("%d", &num1);
-	printf("请输入第二个整数");
-	scanf("%d", &num2);
-	printf("两个整数的和为：%d\n", num1 + num2);*/
-}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	 键盘录入的基本使用
+//	 1.定义一个变量用来接收数据
+//	int a;
+//	 2.键盘录入
+//	printf("请输入一个整数");
+//	scanf("%d", &a);
+//	 3.打印一下
+//	printf("变量a里面的值是：%d\n", a);
+//	char b[]="你好";
+//	printf("%s\n", b);
+//	 定义两个整数类型的变量num1和num2，键盘录入数据分别为两个变量赋值，求两个数的和并进行打印
+//	/*int num1;
+//	int num2;
+//	printf("请输入第一个整数");
+//	scanf("%d", &num1);
+//	printf("请输入第二个整数");
+//	scanf("%d", &num2);
+//	printf("两个整数的和为：%d\n", num1 + num2);*/
+//}
