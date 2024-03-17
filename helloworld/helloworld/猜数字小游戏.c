2ebd443abd4afce9db80e8e7e366274d070e840e
暴龙战士 <63136115+122195@users.noreply.github.com>
2024-03-17 22:19:39 +0800

    2024-3-17

---
+++ helloworld/helloworld/猜数字小游戏.c
@@ -0,0 +1,26 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <time.h>
+//
+//int main() {
+//	srand(time(NULL));
+//
+//	int num = rand() % 100 +1;
+//
+//	int guess;
+//
+//	while (1) {
+//		printf("请输入您要猜的数字\n");
+//		scanf("%d\n",&guess);
+//		if (num > guess) {
+//			printf("小了\n");
+//		}
+//		else if (num < guess) {
+//			printf("大了\n");
+//		}
+//		else {
+//			printf("中了\n");
+//			break;
+//		}
+//	}
+//}
--- helloworld/helloworld/随机数.c
+++ helloworld/helloworld/随机数.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h> // 随机数
-
-int main() {
-	// 1. 设置种子，初始值，因为每一个随机数都是通过前一个数字再结合一系列复杂的计算得到的
-	srand(1);
-	for (int i = 1; i <= 5; i++) {
-		// 2. 设置随机数
-		int num = rand();
-		printf("%d\n", num);
-	}
-}
+//#include <stdio.h>
+//#include <stdlib.h> // 随机数
+//#include <time.h>
+//
+//int main() {
+//	// 1. 设置种子，初始值，因为每一个随机数都是通过前一个数字再结合一系列复杂的计算得到的
+//	//srand(1);
+//	//for (int i = 1; i <= 5; i++) {
+//	//	// 2. 设置随机数
+//	//	int num = rand();
+//	//	printf("%d\n", num);
+//	//}
+//	//绝招：用于生成任意范围之内的随机数
+//	// 把这个范围变成包头不包尾，包左不包右的 8－50
+//	// 拿着尾巴 - 开头 50 - 8 = 42
+//	// 修改代码
+//	// 12-87
+//	srand(time(NULL));
+//	int num = rand() % 76 + 12;
+//	printf("%d\n", num);
+//}
